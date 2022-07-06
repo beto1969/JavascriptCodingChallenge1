d6080a003d0d5f0dff27ac8d98772671c04d054a
beto1969 <coolmath27@hotmail.com>
2022-07-06 00:00:34 -0400

    done

--- Coding challenge (intro).js
+++ Coding challenge (intro).js
@@ -66,17 +66,18 @@ if(operator === "+") {
     console.log(`${num1} * ${num2} = ${multiply(num1, num2)}`);
 } else if(operator === "/") {
     console.log(`${num1} / ${num2} = ${divide(num1, num2)}`);
-=======
-console.log("Hello World");
 
-var myPod= ["Beverlyn Medina", "Derek Munoz", "Thayli Martinez-Dormez ","Humberto Aguilar Sanchez","Lucy"];
+    console.log("Hello World");
 
-console.log(`${myPod[3]} has ${myPod.length} people in his pod`);
+    var myPod = ["Beverlyn Medina", "Derek Munoz", "Thayli Martinez-Dormez ", "Humberto Aguilar Sanchez", "Lucy"];
+
+    console.log(`${myPod[3]} has ${myPod.length} people in his pod`);
 
 
-if (myPod.includes("Lucy")){
-    console.log("Lucy is my POD leader");
-}else {
-    console.log("Lucy is not my POD leader");
+    if (myPod.includes("Lucy")) {
+        console.log("Lucy is my POD leader");
+    } else {
+        console.log("Lucy is not my POD leader");
 
+    }
 }
